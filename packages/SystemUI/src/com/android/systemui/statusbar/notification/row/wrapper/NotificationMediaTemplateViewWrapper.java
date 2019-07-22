368bf8b1ae04643bd47ccbf31d2f5ef07a893a5f
Weijie Wang <weijiew@codeaurora.org>
2019-07-22 19:36:13 +0800

    SystemUI: Update the media notification only when needed
    The media notification is updated every second even if the
    screen is turned off, which causes unnecessary power consumption.
    So update the media notification only when needed
    
    Change-Id: I4d5bcc56fe804754eebec92d83397dac354ec012
    CRs-Fixed: 2493218

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
@@ -32,6 +32,7 @@ import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewStub;
+import android.view.ViewTreeObserver;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -261,6 +262,19 @@ public class NotificationMediaTemplateViewWrapper extends NotificationTemplateVi
                 }
             }, 0, PROGRESS_UPDATE_INTERVAL);
         }
+        updateSeekBarView();
+    }
+
+    private void updateSeekBarView() {
+        mSeekBarView.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener(){
+            @Override
+            public boolean onPreDraw() {
+                mSeekBarView.getViewTreeObserver().removeOnPreDrawListener(this);
+                mHandler.postDelayed(mOnUpdateTimerTick, PROGRESS_UPDATE_INTERVAL);
+                return true;
+            }
+        });
     }
 
     private void clearTimer() {
@@ -318,6 +332,7 @@ public class NotificationMediaTemplateViewWrapper extends NotificationTemplateVi
         public void run() {
             if (mMediaController != null && mSeekBar != null) {
                 PlaybackState playbackState = mMediaController.getPlaybackState();
+                updateSeekBarView();
                 if (playbackState != null) {
                     updatePlaybackUi(playbackState);
                 } else {
